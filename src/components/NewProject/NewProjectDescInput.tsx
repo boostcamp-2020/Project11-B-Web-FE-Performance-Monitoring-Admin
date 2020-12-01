f915f2a06c9344447cf47afc0d62fbf96bf6cf4a
junsushin-dev <junsushin4546@gmail.com>
2020-12-01 22:19:20 +0900

    [feat] 프로젝트 생성 Step 컴포넌트 추가
    - NewProjectNameInput
    - NewProjectDescInput
    - NewProjectConfirm

---
+++ src/components/NewProject/NewProjectDescInput.tsx
@@ -0,0 +1,51 @@
+import React, { useState, useEffect } from 'react';
+import { Box, TextField, styled } from '@material-ui/core';
+
+import BackNextButtons from './BackNextButtons';
+
+const CustomTextField = styled(TextField)({
+  maxWidth: '500px',
+});
+
+interface IProps {
+  desc: string;
+  setDesc: (desc: string) => void;
+  handleBack: () => void;
+  handleNext: () => void;
+}
+
+function NewProjectDescInput(props: IProps): React.ReactElement {
+  const { desc, setDesc, handleBack, handleNext } = props;
+  const labelText = 'Project description';
+  const [inputText, setInputText] = useState('');
+
+  const handleChange = (event: any) => {
+    setInputText(event.target.value);
+  };
+
+  const handleClick = () => {
+    if (inputText === '') return;
+    setDesc(inputText);
+    handleNext();
+  };
+
+  useEffect(() => {
+    setInputText(desc);
+  }, []);
+
+  return (
+    <Box pt={1} display="flex" flexDirection="column">
+      <CustomTextField
+        multiline
+        label={labelText}
+        rows={3}
+        variant="outlined"
+        onChange={handleChange}
+        defaultValue={desc}
+      />
+      <BackNextButtons handleBack={handleBack} handleNext={handleClick} />
+    </Box>
+  );
+}
+
+export default NewProjectDescInput;
