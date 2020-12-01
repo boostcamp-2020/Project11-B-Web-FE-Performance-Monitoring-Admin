ddb2799b040bc2a7fafa6a4e13e47e1d14b53748
HojinLee <hojin5633@gmail.com>
2020-12-01 23:17:21 +0900

    [feat] 로그인 화면 디자인 추가
    - 로고와 버튼으로 간단한 디자인 구현

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,4 +1,24 @@
 import React from 'react';
+import Grid from '@material-ui/core/Grid';
+import GitHubIcon from '@material-ui/icons/GitHub';
+import Button from '@material-ui/core/Button';
+import { makeStyles } from '@material-ui/core/styles';
+import PanopticonLogo from '../image/panopticon.png';
+
+const useStyles = makeStyles((theme) => ({
+  grid: {
+    marginTop: '20px',
+  },
+  button: {
+    margin: theme.spacing(5),
+    textTransform: 'none',
+    fontSize: '30px',
+    fontWeight: 600,
+  },
+  icon: {
+    marginRight: '10px',
+  },
+}));
 
 const isDev: boolean = process.env.NODE_ENV === 'development';
 // const oauthURL: string = isDev ? 'http://panopticon-dev.gq/api/auth/github' : '/api/auth/github';
@@ -6,11 +26,22 @@ const oauthURL: string = isDev ? 'http://localhost:3000/api/auth/github' : '/api
 
 const Login = (): React.ReactElement => {
   window.history.replaceState(null, '', '/login');
+
+  const classes = useStyles();
   return (
-    <div>
-      <h2>Panopticon</h2>
-      <a href={oauthURL}>구글 로그인하기</a>
-    </div>
+    <Grid
+      container
+      direction="column"
+      justify="flex-start"
+      alignItems="center"
+      className={classes.grid}
+    >
+      <img src={PanopticonLogo} alt="logo" />
+      <Button variant="contained" color="default" className={classes.button}>
+        <GitHubIcon fontSize="inherit" className={classes.icon} />
+        <a href={oauthURL}>Login With Github</a>
+      </Button>
+    </Grid>
   );
 };
 export default Login;
