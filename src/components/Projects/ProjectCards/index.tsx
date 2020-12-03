5fe2a2169b8b882083c345c1b29102b853c7813b
junsushin-dev <junsushin4546@gmail.com>
2020-12-03 17:14:13 +0900

    [feat] Projects 페이지 API 연동 추가
    - 서비스 함수 이용해서 함수 가져오기 추가
    - 샘플 데이터 형식 변경
    - DSN string wrapping 되는 것으로 변경

--- src/components/Projects/ProjectCards/index.tsx
+++ src/components/Projects/ProjectCards/index.tsx
@@ -1,40 +1,100 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Box, Grid } from '@material-ui/core';
 
-import ProjectCard from './ProjectCard';
+import ProjectCard, { IProjectCardProps } from './ProjectCard';
+import service from '../../../service';
 
-const projects = [
+const sampleProjects: IProjectCardProps[] = [
   {
+    _id: '5fc6026191611635d8983358',
     name: 'MyProject1',
-    dsn: 'panopticon.gq/api/errors/MyProject1',
-    owner: 'junsushin-dev',
-    members: [],
+    owner: {
+      _id: '5fc055d7d58baa2b2807974b',
+      uid: 55068119,
+      nickname: 'junsushin-dev',
+      email: 'junsushin4546@gmail.com',
+    },
+    users: [
+      {
+        _id: '5fc5d0f738d1839a9be86541',
+        uid: 55068119,
+        email: 'earlyhail6@gmail.com',
+        nickname: 'EarlyHail',
+      },
+    ],
   },
   {
+    _id: '5fc6026191611635d8983356',
     name: 'MyProject2',
-    dsn: 'panopticon.gq/api/errors/MyProject2',
-    owner: 'junsushin-dev',
-    members: ['saeeng', 'juyoungpark', 'EarlyHail'],
+    owner: {
+      _id: '5fc7033438d1839a9b14bd79',
+      uid: 32405358,
+      email: 'junsushin4546@gmail.com',
+      nickname: 'junsushin-dev',
+    },
+    users: [
+      {
+        _id: '5fc5d0f738d1839a9be86541',
+        uid: 55068119,
+        email: 'earlyhail6@gmail.com',
+        nickname: 'EarlyHail',
+      },
+      {
+        _id: '5fc7205838d1839a9b191373',
+        uid: 41819176,
+        email: 'saeeng@gmail.com',
+        nickname: 'saeeng',
+      },
+    ],
   },
   {
+    _id: '5fc6026191611635d8983357',
     name: 'MyProject3',
-    dsn: 'panopticon.gq/api/errors/MyProject3',
-    owner: 'saeeng',
-    members: ['juyoungpark', 'EarlyHail'],
+    owner: {
+      _id: '5fc7033438d1839a9b14bd79',
+      uid: 32405358,
+      email: 'junsushin4546@gmail.com',
+      nickname: 'junsushin-dev',
+    },
+    users: [
+      {
+        _id: '5fc5d0f738d1839a9be86541',
+        uid: 55068119,
+        email: 'earlyhail6@gmail.com',
+        nickname: 'EarlyHail',
+      },
+      {
+        _id: '5fc7205838d1839a9b191373',
+        uid: 41819176,
+        email: 'saeeng@gmail.com',
+        nickname: 'saeeng',
+      },
+    ],
   },
 ];
 
 function ProjectCards(): React.ReactElement {
+  const [projects, setProjects] = useState<IProjectCardProps[]>([]);
+
+  useEffect(() => {
+    // TODO: Projects API와 연동 테스트
+    // (async () => {
+    //   const response = await service.getProjects();
+    //   setProjects(response.data.projects);
+    // })();
+    setProjects(sampleProjects);
+  });
+
   return (
     <Box pt={2}>
       <Grid container spacing={3}>
         {projects.map((props) => (
           <ProjectCard
-            key={props.dsn}
+            key={props._id}
+            _id={props._id}
             name={props.name}
-            dsn={props.dsn}
             owner={props.owner}
-            members={props.members}
+            users={props.users}
           />
         ))}
       </Grid>
