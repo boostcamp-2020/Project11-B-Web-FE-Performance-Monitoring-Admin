b60e30315173b04da79f890e0000ca44f9d1354a
juyoungpark718 <juyoung7018.park@gmail.com>
2020-12-04 03:21:13 +0900

    [feat] login page 컴포넌트에 social login 추가

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,9 +1,12 @@
-import React from 'react';
+import React, { useState, useRef, useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
 import Grid from '@material-ui/core/Grid';
 import GitHubIcon from '@material-ui/icons/GitHub';
 import Button from '@material-ui/core/Button';
 import { makeStyles } from '@material-ui/core/styles';
+import qs from 'qs';
 import PanopticonLogo from '../image/panopticon.png';
+import service from '../service';
 
 const useStyles = makeStyles((theme) => ({
   grid: {
@@ -20,10 +23,78 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-const OAUTH_URL = '/api/auth/github';
+type IWindowProps = {
+  url: string;
+  title: string;
+  width: number;
+  height: number;
+};
 
 const Login = (): React.ReactElement => {
-  window.history.replaceState(null, '', '/login');
+  const OAUTH_URL = `https://github.com/login/oauth/authorize?client_id=${
+    process.env.NODE_ENV === 'development'
+      ? process.env.REACT_APP_GITHUB_OAUTH_DEV_CLIENT_ID
+      : process.env.REACT_APP_GITHUB_OAUTH_CLIENT_ID
+  }&scope=user:email`;
+
+  const [externalWindow, setExternalWindow] = useState<Window | null>();
+  const intervalRef = useRef<number>();
+  const history = useHistory();
+
+  const clearTimer = () => {
+    window.clearInterval(intervalRef.current);
+  };
+
+  const createPopup = ({ url, title, height, width }: IWindowProps) => {
+    const left = window.screenX + (window.outerWidth - width) / 2;
+    const top = window.screenY + (window.outerHeight - height) / 2.5;
+    const externalPopup = window.open(
+      url,
+      title,
+      `width=${width},height=${height},left=${left},top=${top}`,
+    );
+    return externalPopup;
+  };
+
+  const handleClick = () => {
+    setExternalWindow(
+      createPopup({
+        url: OAUTH_URL,
+        title: '',
+        width: 500,
+        height: 600,
+      }),
+    );
+  };
+
+  useEffect(() => {
+    if (externalWindow) {
+      intervalRef.current = window.setInterval(async () => {
+        try {
+          const currentUrl = externalWindow.location.search;
+          const { code } = qs.parse(currentUrl, { ignoreQueryPrefix: true });
+          if (!code) return;
+          clearTimer();
+          const { data } = await service.login(code as string);
+          if (!data.token || !data.nickname) {
+            history.replace('/login');
+          } else {
+            localStorage.setItem('token', data.token);
+            localStorage.setItem('nickname', data.token);
+            history.replace('/');
+            externalWindow.close();
+          }
+          // eslint-disable-next-line no-empty
+        } catch (e) {
+        } finally {
+          if (!externalWindow || externalWindow.closed) {
+            externalWindow.close();
+            clearTimer();
+          }
+        }
+      }, 1000);
+    }
+  });
 
   const classes = useStyles();
   return (
@@ -35,9 +106,9 @@ const Login = (): React.ReactElement => {
       className={classes.grid}
     >
       <img src={PanopticonLogo} alt="logo" />
-      <Button variant="contained" color="default" className={classes.button}>
+      <Button onClick={handleClick} variant="contained" color="default" className={classes.button}>
         <GitHubIcon fontSize="inherit" className={classes.icon} />
-        <a href={OAUTH_URL}>Login With Github</a>
+        Login With Github
       </Button>
     </Grid>
   );
