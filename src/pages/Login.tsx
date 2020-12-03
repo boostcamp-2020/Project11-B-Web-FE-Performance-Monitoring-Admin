e5013982ed498e39cd5b1b25f0ea3aa7fdb770fa
juyoungpark718 <juyoung7018.park@gmail.com>
2020-12-04 04:40:28 +0900

    [fix] router에서 login을 메인으로 수정
    - login style 수정

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,25 +1,39 @@
-import React, { useState, useRef, useEffect } from 'react';
+import React, { useState, useRef, useEffect, useContext } from 'react';
 import { useHistory } from 'react-router-dom';
 import Grid from '@material-ui/core/Grid';
+import Box from '@material-ui/core/Box';
 import GitHubIcon from '@material-ui/icons/GitHub';
 import Button from '@material-ui/core/Button';
+import Paper from '@material-ui/core/Paper';
 import { makeStyles } from '@material-ui/core/styles';
 import qs from 'qs';
 import PanopticonLogo from '../image/panopticon.png';
 import service from '../service';
+import UserContext from '../context';
 
 const useStyles = makeStyles((theme) => ({
   grid: {
     marginTop: '20px',
   },
   button: {
-    margin: theme.spacing(5),
+    backgroundColor: 'black',
+    margin: theme.spacing(4),
     textTransform: 'none',
-    fontSize: '30px',
+    fontSize: '20px',
+    color: 'white',
     fontWeight: 600,
+    '&:hover': {
+      backgroundColor: 'white',
+      color: 'black',
+    },
   },
   icon: {
     marginRight: '10px',
+  },
+  img: {
+    width: '240px',
+    height: 'auto',
+    margin: '0 auto',
   },
 }));
 
@@ -31,6 +45,7 @@ type IWindowProps = {
 };
 
 const Login = (): React.ReactElement => {
+  const { setUser } = useContext(UserContext);
   const OAUTH_URL = `https://github.com/login/oauth/authorize?client_id=${
     process.env.NODE_ENV === 'development'
       ? process.env.REACT_APP_GITHUB_OAUTH_DEV_CLIENT_ID
@@ -77,11 +92,15 @@ const Login = (): React.ReactElement => {
           clearTimer();
           const { data } = await service.login(code as string);
           if (!data.token || !data.nickname) {
-            history.replace('/login');
-          } else {
-            localStorage.setItem('token', data.token);
-            localStorage.setItem('nickname', data.token);
             history.replace('/');
+          } else {
+            const { nickname, token } = data;
+            localStorage.setItem('token', token);
+            localStorage.setItem('nickname', nickname);
+            if (setUser) {
+              setUser({ nickname, token });
+            }
+            history.replace('/projects');
             externalWindow.close();
           }
           // eslint-disable-next-line no-empty
@@ -105,11 +124,17 @@ const Login = (): React.ReactElement => {
       alignItems="center"
       className={classes.grid}
     >
-      <img src={PanopticonLogo} alt="logo" />
-      <Button onClick={handleClick} variant="contained" color="default" className={classes.button}>
-        <GitHubIcon fontSize="inherit" className={classes.icon} />
-        Login With Github
-      </Button>
+      <Box height="100vh" pt={20}>
+        <Paper elevation={3}>
+          <Box display="flex" flexDirection="column" p={10}>
+            <img className={classes.img} src={PanopticonLogo} alt="logo" />
+            <Button onClick={handleClick} variant="contained" className={classes.button}>
+              <GitHubIcon fontSize="inherit" className={classes.icon} />
+              Sign in With GitHub
+            </Button>
+          </Box>
+        </Paper>
+      </Box>
     </Grid>
   );
 };
