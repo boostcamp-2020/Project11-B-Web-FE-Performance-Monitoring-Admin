97778a37d7fab7df573efea602caaacc121676a9
junsushin-dev <junsushin4546@gmail.com>
2020-12-07 18:21:20 +0900

    [feat] 테스트용 getCrime API service 파일 생성
    - 임시 데이터 조회용 service 함수 추가

--- src/service/issueService.ts
+++ src/service/issueService.ts
@@ -8,6 +8,8 @@ export default (
 ): {
   getIssue: (id: string) => Promise<AxiosResponse>;
   getIssues: (query: string) => Promise<AxiosResponse>;
+  // getCrime: (id: string) => Promise<AxiosResponse>;
+  getCrime: (id: string) => Promise<any>;
 } => {
   const getIssue = (id: string) => {
     return apiRequest.get(`/api/issue/${id}`);
@@ -17,8 +19,49 @@ export default (
     return apiRequest.get(`/api/issues${query}`);
   };
 
+  const getCrime = async (id: string) => {
+    /*
+     ** @TODO
+     ** 단일 Crime API 연동
+     */
+    // return apiRequest.get(`/api/crime/${id}`);
+    return {
+      _id: '5fcc8ced5931b8932cca0753',
+      meta: {
+        browser: {
+          name: 'fire-fox2222222222',
+          version: 'recent',
+        },
+        os: {
+          name: 'windows10',
+          version: '1909',
+        },
+        url: '유저 url 1번',
+        ip: 'localhost:4000',
+      },
+      message: '이슈 1번',
+      type: '이슈 1번',
+      stack: [
+        {
+          _id: '5fcc8ced5931b8932cca0754',
+          columnNo: '81',
+          lineNo: '110',
+          function: 'occurError()',
+          filename: 'index.js',
+        },
+      ],
+      occuredAt: '2020-11-26T09:09:30.000Z',
+      sdk: {
+        name: 'panopticon',
+        version: '1.0.0',
+      },
+      __v: 0,
+    };
+  };
+
   return {
     getIssue,
     getIssues,
+    getCrime,
   };
 };
