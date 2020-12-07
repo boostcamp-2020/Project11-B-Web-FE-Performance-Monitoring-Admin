0ee8df304d21269136b3e86c60538ac2685ef27f
junsushin-dev <junsushin4546@gmail.com>
2020-12-07 23:12:33 +0900

    [feat] IssueDetail 페이지에 CrimeView 컴포넌트 추가
    - IssueDetail 페이지 변경 이전 CrimeView 보는 용도로 추가
    - 중복되는 이전 IssueDetail 페이지 부분 삭제 (Tag, Stack)

--- src/pages/IssueDetail.tsx
+++ src/pages/IssueDetail.tsx
@@ -1,8 +1,7 @@
 import React, { useState, useEffect, useContext } from 'react';
 import { useRouteMatch } from 'react-router-dom';
 import { Box } from '@material-ui/core';
-import Tag from '../components/Tag';
-import Stack from '../components/Stack';
+import CrimeView from '../components/IssueDetail/CrimeView';
 import service from '../service';
 import UserContext from '../context';
 
@@ -35,6 +34,7 @@ interface IssueType {
     url: string;
     ip: string;
   };
+  errorIds: string[];
 }
 
 interface MatchParams {
@@ -64,44 +64,7 @@ function IssueDetail(): React.ReactElement {
         </Box>
         <Box fontSize="16px">{issue && issue.stack[0].filename}</Box>
       </Box>
-      <Box borderBottom="1px solid #E2DEE6">
-        <Box px={4} py={4}>
-          <Box color="#9386A0" pb={2}>
-            TAGS
-          </Box>
-          <Box display="flex" flexWrap="wrap" gridGap={10}>
-            {issue
-              ? (() => {
-                  return (
-                    <>
-                      <Tag name={issue.meta.browser.name} content={issue.meta.browser.version} />
-                      <Tag name={issue.meta.os.name} content={issue.meta.os.version} />
-                      <Tag name="URL" content={issue.meta.url} />
-                      <Tag name="IP" content={issue.meta.ip} />
-                    </>
-                  );
-                })()
-              : null}
-
-            <Tag name="browser" content="Chrome 86.0.4240" />
-          </Box>
-        </Box>
-      </Box>
-      <Box>
-        <Box px={4} py={2}>
-          <Box>
-            <Box>
-              <Box fontSize="20px" fontWeight={500}>
-                ReferenceError
-              </Box>
-              <Box>undefiendMethod is not defined</Box>
-            </Box>
-          </Box>
-          <Box mt={3} border="1px solid #9386A0" borderRadius="3px">
-            {issue && issue.stack.map((stack) => <Stack key={stack._id} stack={stack} />)}
-          </Box>
-        </Box>
-      </Box>
+      {issue && <CrimeView errorIds={issue.errorIds} />}
     </Box>
   );
 }
