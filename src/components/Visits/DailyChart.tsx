83f6e2ef2dd8a29af654d75a143dde3fd245d62d
EarlyHail <hojin5633@gmail.com>
2020-12-12 22:40:17 +0900

    [refactor] 일간 방문자 수 Table 리팩토링
    - 일간 방문자 수 Table 리팩토링
       Table을 Component로 분리

--- src/components/Visits/DailyChart.tsx
+++ src/components/Visits/DailyChart.tsx
@@ -1,26 +1,20 @@
 import React, { useEffect, useRef } from 'react';
 import { useSelector } from 'react-redux';
 import 'billboard.js/dist/billboard.css';
-import Box from '@material-ui/core/Box';
-import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
-import TableContainer from '@material-ui/core/TableContainer';
-import TableHead from '@material-ui/core/TableHead';
-import TableRow from '@material-ui/core/TableRow';
-import Paper from '@material-ui/core/Paper';
-import { RootState } from '../../modules';
-import drawVisitsChart from '../../utils/visitUtil';
 
+import { RootState } from '../../modules';
+import { IDailyVisit } from '../../types';
+import drawVisitsChart from '../../utils/visitUtil';
 import service from '../../service';
 
 interface IProps {
   year: number;
   month: number;
+  setFirstSelectedCounts: (newDailyVisits: IDailyVisit[]) => void;
 }
 
 function DailyChart(props: IProps): React.ReactElement {
-  const { year, month }: IProps = props;
+  const { year, month, setFirstSelectedCounts }: IProps = props;
   const projects = useSelector((state: RootState) => state.projects.projects);
   const selectedProjectsIds = useSelector((state: RootState) => state.projects.selectedProjectsIds);
   const visitChartDiv = useRef(null);
@@ -28,41 +22,13 @@ function DailyChart(props: IProps): React.ReactElement {
     (async (): Promise<void> => {
       const dailyRes = await service.getDailyVisitsMulti(selectedProjectsIds, year, month);
       const newDailyVisits = await dailyRes.data;
+      setFirstSelectedCounts(newDailyVisits[0]);
       drawVisitsChart({ projects, newVisits: newDailyVisits, visitChartDiv, type: 'daily' });
     })();
   }, [selectedProjectsIds, year, month]);
   return (
     <>
       <div ref={visitChartDiv} />
-      <Box>
-        <TableContainer component={Paper}>
-          <Table size="small" aria-label="a dense table">
-            <TableHead>
-              <TableRow>
-                <TableCell>Year</TableCell>
-                <TableCell align="right">Month</TableCell>
-                <TableCell align="right">Date</TableCell>
-                <TableCell align="right">Count</TableCell>
-                <TableCell align="right">Average Stay time</TableCell>
-              </TableRow>
-            </TableHead>
-            {/* <TableBody>
-              {dailyVisits.map((dailyVisit) => (
-                <TableRow
-                  key={`${dailyVisit._id.year}-${dailyVisit._id.month}-${dailyVisit._id.date}`}
-                >
-                  <TableCell component="th" scope="row">
-                    {dailyVisit._id.year}
-                  </TableCell>
-                  <TableCell align="right">{dailyVisit._id.month}</TableCell>
-                  <TableCell align="right">{dailyVisit._id.date}</TableCell>
-                  <TableCell align="right">{dailyVisit.count}</TableCell>
-                </TableRow>
-              ))}
-            </TableBody> */}
-          </Table>
-        </TableContainer>
-      </Box>
     </>
   );
 }
--- src/pages/Visits.tsx
+++ src/pages/Visits.tsx
@@ -3,11 +3,13 @@ import { Box } from '@material-ui/core';
 import VisitsHeader from '../components/Visits/VisitsHeader';
 import MonthlyChart from '../components/Visits/MonthlyChart';
 import DailyChart from '../components/Visits/DailyChart';
+import DailyTable from '../components/Visits/DailyTable';
 import ProjectSelector from '../components/Issues/ProjectSelector';
+import { IDailyVisit } from '../types';
 
 function Visits(): React.ReactElement {
   const today = new Date();
-  const projectId = '5fd0bbb03eaa461e2c83a0c4';
+  const [firstSelectedCounts, setFirstSelectedCounts] = useState<IDailyVisit[]>([]);
   const [year, setYear] = useState(today.getFullYear());
   const [month, setMonth] = useState(today.getMonth() + 1);
   const nextMonth = () => {
@@ -26,7 +28,6 @@ function Visits(): React.ReactElement {
       setMonth(month - 1);
     }
   };
-
   return (
     <Box p={3}>
       <ProjectSelector />
@@ -36,7 +37,10 @@ function Visits(): React.ReactElement {
           <MonthlyChart year={year} />
         </Box>
         <Box>
-          <DailyChart year={year} month={month} />
+          <DailyChart year={year} month={month} setFirstSelectedCounts={setFirstSelectedCounts} />
+        </Box>
+        <Box>
+          <DailyTable dailyVisits={firstSelectedCounts} />
         </Box>
       </Box>
     </Box>
