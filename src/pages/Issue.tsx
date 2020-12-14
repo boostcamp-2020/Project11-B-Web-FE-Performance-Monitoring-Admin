5c50bb576f8ad038fb074580c19036cf67444774
saeeng <saeeng.opt@gmail.com>
2020-12-15 03:29:32 +0900

    [refactor] issue페이지 filter Page 적용
    - issue페이지에 filter Page 적용

--- src/pages/Issue.tsx
+++ src/pages/Issue.tsx
@@ -1,31 +1,18 @@
-import React, { useState } from 'react';
-import { Box, Grid } from '@material-ui/core';
+import React from 'react';
+import { Box } from '@material-ui/core';
 import IssueTable from '../components/Issues/IssueTable';
 import IssueHeader from '../components/Issues/IssuesHeader';
-import ProjectSelector from '../components/common/ProjectSelector';
-import PeriodSelector from '../components/common/PeriodSelector';
+import FilterPage from '../components/layout/FilterPage';
 
 function Issue(): React.ReactElement {
-  const [period, setPeriod] = useState<string>('1w');
   return (
     <>
-      <Box p={3}>
-        <Box width="100%">
-          <Grid container spacing={2}>
-            <Grid item xs={6}>
-              <ProjectSelector />
-            </Grid>
-            <Grid item xs={6}>
-              <PeriodSelector period={period} all setPeriod={setPeriod} />
-            </Grid>
-          </Grid>
-        </Box>
-
+      <FilterPage>
         <Box pt={3}>
           <IssueHeader />
-          <IssueTable period={period} />
+          <IssueTable />
         </Box>
-      </Box>
+      </FilterPage>
     </>
   );
 }
