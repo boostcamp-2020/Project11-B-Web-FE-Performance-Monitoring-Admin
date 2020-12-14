e4f6c5581c84a1342efb11a673ca9393c2c60486
saeeng <saeeng.opt@gmail.com>
2020-12-15 01:28:23 +0900

    [feat] issues에 기간 필터 적용
    - 기간별 필터를 적용시켰습니다.
    - ui를 정리했습니다.

--- src/pages/Issue.tsx
+++ src/pages/Issue.tsx
@@ -1,18 +1,32 @@
-import React from 'react';
-import { Box } from '@material-ui/core';
+import React, { useState } from 'react';
+import { Box, Grid } from '@material-ui/core';
 import IssueTable from '../components/Issues/IssueTable';
 import IssueHeader from '../components/Issues/IssuesHeader';
-import ProjectSelector from '../components/Issues/ProjectSelector';
+import ProjectSelector from '../components/common/ProjectSelector';
+import PeriodSelector from '../components/common/PeriodSelector';
 
 function Issue(): React.ReactElement {
+  const [period, setPeriod] = useState<string>('1w');
   return (
-    <Box p={3}>
-      <ProjectSelector />
-      <Box pt={3}>
-        <IssueHeader />
-        <IssueTable />
+    <>
+      <Box p={3}>
+        <Box width="100%">
+          <Grid container spacing={2}>
+            <Grid item xs={6}>
+              <ProjectSelector />
+            </Grid>
+            <Grid item xs={6}>
+              <PeriodSelector period={period} all setPeriod={setPeriod} />
+            </Grid>
+          </Grid>
+        </Box>
+
+        <Box pt={3}>
+          <IssueHeader />
+          <IssueTable period={period} />
+        </Box>
       </Box>
-    </Box>
+    </>
   );
 }
 
