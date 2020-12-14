36484d87ac9308aac28d6f801d376c3edbbe2a94
EarlyHail <hojin5633@gmail.com>
2020-12-14 22:28:46 +0900

    [feat] axios 에러 처리
    - 401, 502에러
       세션 만료 알림, 로그아웃 처리 후 메인페이지 리다이렉팅
    
    - 400, 404, 500
      에러 페이지로 리다이렉팅
    
    - 나머지 에러
       로그아웃 처리 후 메인 페이지로 리다이렉팅

--- src/utils/logoutUser.ts
+++ src/utils/logoutUser.ts
@@ -2,6 +2,5 @@ const logoutUser = (): void => {
   // eslint-disable-next-line react-hooks/rules-of-hooks
   localStorage.removeItem('token');
   localStorage.removeItem('nickname');
-  window.location.href = '/';
 };
 export default logoutUser;
