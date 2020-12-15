b70837147bc7f0859f1d12060d01966681ec3eb5
juyoungpark718 <juyoung7018.park@gmail.com>
2020-12-16 06:02:10 +0900

    [fix] AlertsConfig create button 작동하게 바꿈
    - alerts에서 상태값을 커스텀 훅을 이용해서 넘겨 받는 것으로 수정

--- src/components/Alerts/AlertsConfig.tsx
+++ src/components/Alerts/AlertsConfig.tsx
@@ -4,14 +4,20 @@ import Button from '@material-ui/core/Button';
 import UserList from './UserList';
 import { IAlertsUserProfile } from '../../types';
 import TimerBtn from '../common/TimerBtn';
+import { UserAlertSelector } from '../../hooks/AlertHooks';
 
 interface IProps {
+  selector: UserAlertSelector;
   userList?: IAlertsUserProfile[];
   handleUserList: (email: string) => void;
+  handleSubmit: (users: string[]) => () => Promise<void>;
 }
 
 function AlertsConfig(props: IProps): React.ReactElement {
-  const { userList, handleUserList } = props;
+  const { userList, handleUserList, handleSubmit, selector } = props;
+  const { project, period, count } = selector((prev) => prev);
+  const selectedUserList = userList?.filter((user) => user.isSelected).map(({ _id }) => _id);
+  const canSubmission = Boolean(project && (period || count) && selectedUserList?.length);
   return (
     <Box>
       <Box display="flex" justifyContent="space-between" alignItems="center" px={2} mb={1}>
@@ -22,7 +28,13 @@ function AlertsConfig(props: IProps): React.ReactElement {
         <UserList handleUserList={handleUserList} userList={userList} />
       </Box>
       <Box display="flex" alignItems="center" justifyContent="flex-end" mt={2}>
-        <Button variant="contained" color="primary">
+        <Button
+          type="button"
+          onClick={handleSubmit(selectedUserList || [])}
+          variant="contained"
+          color="primary"
+          disabled={!canSubmission}
+        >
           Create Alert
         </Button>
       </Box>
--- src/components/Alerts/UserList/UserProfile.tsx
+++ src/components/Alerts/UserList/UserProfile.tsx
@@ -4,7 +4,7 @@ import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles({
   chip: {
-    margin: '0 8px',
+    margin: '4px 8px',
   },
 });
 
