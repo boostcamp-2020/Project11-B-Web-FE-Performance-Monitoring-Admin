6f0d64841373e3e780fb59596441b2b5f4a420ff
saeeng <saeeng.opt@gmail.com>
2020-12-17 02:06:58 +0900

    [fix] 이미지 로드 안되던 현상 해결
    - 별도의 firestorage를 사용하여 주소 변경
    - feature의 gif도 주소가 만료되어 원본 주소로 변경
    - 메인페이지 로드시 화질이 낮은 사진 우선 로드 후 고화질 파일로 교체

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,4 +1,4 @@
-import React, { Suspense } from 'react';
+import React from 'react';
 import { Box, AppBar, Toolbar, useScrollTrigger, Typography } from '@material-ui/core';
 
 import Container from '@material-ui/core/Container';
@@ -7,6 +7,7 @@ import MainIntroduce from '../components/Main/MainIntroduce';
 import PanLogo from '../image/pan-logo-black.png';
 import PanLogoWhite from '../image/pan-logo-white.png';
 import Login from '../components/Main/Login';
+import useDelayedImg from '../hooks/DelayedImgHooks';
 
 interface IProps {
   children: React.ReactElement;
@@ -22,15 +23,13 @@ function ElevationScroll(props: IProps) {
 
   return React.cloneElement(children, {
     elevation: trigger ? 4 : 0,
-    color: trigger ? 'white' : 'transparent',
+    color: trigger ? 'inherit' : 'transparent',
     src: trigger ? PanLogo : PanLogoWhite,
   });
 }
 
 const useStyles = makeStyles({
   mainDescription: {
-    // backgroundImage: `url(https://pixabay.com/get/57e8d4444352ac14f6d1867dda3536781537dee75253724b_1920.jpg)`,
-    backgroundImage: `url(https://pixabay.com/get/54e8d64b4350ad14ead98d78c42f3e791222dfe35359714d7c2b7dd2.jpg)`,
     backgroundSize: 'cover',
     backgroundPosition: 'center',
   },
@@ -44,47 +43,45 @@ const useStyles = makeStyles({
     background: 'rgba(0,0,0,0.85)',
   },
 });
+const introduces = [
+  {
+    title: 'Collect Errors & Logs from Users',
+    list: ['배포된 웹 App의 사용자들로부터 에러와 접속자, 접속 시간 로그를 실시간으로 수집하세요.'],
+    img: 'https://i.imgur.com/qAVbuIG.gif',
+  },
+  {
+    title: 'Share Your Project with Team Members',
+    list: [
+      '팀원을 초대해서 프로젝트를 공유해보세요.',
+      '여러 프로젝트에 관련된 통합적 모니터링 기능을 통하여 보다 다양한 인사이트를 얻을 수 있습니다.',
+    ],
+    img: 'https://i.imgur.com/qVVrBJJ.gif',
+  },
+  {
+    title: 'Discover Error Trends',
+    list: [
+      '어떤 브라우저, OS, URL에서 에러가 주로 발생하는지 파악해보세요. ',
+      '프로젝트의 에러가 늘어나는지, 줄어드는지 트렌드를 모니터링 할 수 있습니다. ',
+    ],
+    img: 'https://i.imgur.com/i9R1d7w.gif',
+  },
+  {
+    title: 'See How Your Clients Behave',
+    list: [
+      'MAU, DAU를 시각화합니다.',
+      '페이지 간 이동 및 체류 시간에 관련된 데이터 패턴에 대한 모니터링 기능을 제공합니다.',
+    ],
+    img: 'https://i.imgur.com/w1nhwCb.gif',
+  },
+];
 
 function Main(): React.ReactElement {
   const classes = useStyles();
-  const introduces = [
-    {
-      title: 'Collect Errors & Logs from Users',
-      list: [
-        '배포된 웹 App의 사용자들로부터 에러와 접속자, 접속 시간 로그를 실시간으로 수집하세요.',
-      ],
-      img:
-        // 'https://s3.us-west-2.amazonaws.com/secure.notion-static.com/c12fefd8-0f80-4d35-a6a4-4aeb13625e95/bU7LFpvQVj.gif?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAT73L2G45O3KS52Y5%2F20201215%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20201215T082142Z&X-Amz-Expires=86400&X-Amz-Signature=ab6d0794975c1403087c969a47b7f81ad6539e5a38892888d4c318d1ce84933a&X-Amz-SignedHeaders=host',
-        'https://i.imgur.com/qAVbuIG.gif',
-    },
-    {
-      title: 'Share Your Project with Team Members',
-      list: [
-        '팀원을 초대해서 프로젝트를 공유해보세요.',
-        '여러 프로젝트에 관련된 통합적 모니터링 기능을 통하여 보다 다양한 인사이트를 얻을 수 있습니다.',
-      ],
-      img:
-        'https://s3.us-west-2.amazonaws.com/secure.notion-static.com/16b4e3ea-2979-4148-831d-b389169f01eb/0LrkhL0djo.gif?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAT73L2G45O3KS52Y5%2F20201215%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20201215T082142Z&X-Amz-Expires=86400&X-Amz-Signature=2b021d5fa2b7a44a6957c105d0c8130b088cb6f6d04b8fa5b53562d9c00e3fd1&X-Amz-SignedHeaders=host',
-    },
-    {
-      title: 'Discover Error Trends',
-      list: [
-        '어떤 브라우저, OS, URL에서 에러가 주로 발생하는지 파악해보세요. ',
-        '프로젝트의 에러가 늘어나는지, 줄어드는지 트렌드를 모니터링 할 수 있습니다. ',
-      ],
-      img:
-        'https://s3.us-west-2.amazonaws.com/secure.notion-static.com/be7e4f1b-9078-4bfa-82aa-be905403717b/BKcmuN2U49.gif?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAT73L2G45O3KS52Y5%2F20201215%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20201215T082142Z&X-Amz-Expires=86400&X-Amz-Signature=90c522c7c5feb7801f14e7ac91496e65f5be0b2f2c7a7a51bea717b1e3b6fef8&X-Amz-SignedHeaders=host',
-    },
-    {
-      title: 'See How Your Clients Behave',
-      list: [
-        'MAU, DAU를 시각화합니다.',
-        '페이지 간 이동 및 체류 시간에 관련된 데이터 패턴에 대한 모니터링 기능을 제공합니다.',
-      ],
-      img:
-        'https://s3.us-west-2.amazonaws.com/secure.notion-static.com/464de679-14cb-4e45-814f-40e69b172681/MiBZlmAxvs.gif?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAT73L2G45O3KS52Y5%2F20201215%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20201215T082142Z&X-Amz-Expires=86400&X-Amz-Signature=f903f068fcdb5662d884c1743e78af420e805a7d491bba09e1d7a9b1b351e933&X-Amz-SignedHeaders=host',
-    },
-  ];
+  const src =
+    'https://firebasestorage.googleapis.com/v0/b/panopticon-7444c.appspot.com/o/laptop-2838918.jpg?alt=media&token=4a2b7c76-0110-4ff0-b54a-3a5c4ef80be8';
+
+  const loadedSrc = useDelayedImg(src);
+
   return (
     <Box>
       <ElevationScroll>
@@ -112,6 +109,12 @@ function Main(): React.ReactElement {
         height="85vh"
         width="100%"
         className={classes.mainDescription}
+        style={{
+          backgroundImage: `url(${
+            loadedSrc ||
+            'https://firebasestorage.googleapis.com/v0/b/panopticon-7444c.appspot.com/o/laptop-2838918_640.jpg?alt=media&token=97ecb6c7-8a10-4ba4-8714-1f06b76033a9'
+          })`,
+        }}
       />
 
       <Box
@@ -139,6 +142,7 @@ function Main(): React.ReactElement {
         >
           <Box />
           <img width="450px" src={PanLogoWhite} alt="panopticon-logo" />
+
           <Typography color="primary" variant="h2">
             - 웹 APP 개발 에러 및 로그 모니터링 및 분석 플랫폼 -
           </Typography>
@@ -154,9 +158,9 @@ function Main(): React.ReactElement {
             <h1>Feature</h1>
           </Box>
           {introduces.map(({ title, list, img }) => (
-            <Suspense fallback="loading">
-              <MainIntroduce key={title} title={title} list={list} img={img} />
-            </Suspense>
+            <Box key={title}>
+              <MainIntroduce title={title} list={list} img={img} />
+            </Box>
           ))}
         </Box>
       </Container>
