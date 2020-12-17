2ba5ea67ebe497d6cbf05e7b8a3ddba463602ac9
saeeng <saeeng.opt@gmail.com>
2020-12-18 01:02:15 +0900

    [refactor] analysis 데이터 교체
    - usercount 통하여 유저별 평균 시간 표시
    - table 을 datagrid로 변경, pagination 및 정렬 가능

--- src/components/Analysis/DailyDurationTable.tsx
+++ src/components/Analysis/DailyDurationTable.tsx
@@ -1,17 +1,56 @@
 import React from 'react';
-import {
-  Box,
-  Table,
-  Typography,
-  TableBody,
-  TableCell,
-  TableContainer,
-  TableHead,
-  TableRow,
-  Paper,
-} from '@material-ui/core';
+import { Box, Typography } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+import { DataGrid, ColDef, CellParams } from '@material-ui/data-grid';
 
-import arrayToCSV from '../../utils/arrayToCSV';
+const useStyles = makeStyles({
+  table: {
+    bgColor: 'primary',
+    '&.MuiDataGrid-root .MuiDataGrid-colCellTitle': {
+      fontWeight: '600',
+    },
+  },
+});
+const columns: ColDef[] = [
+  { field: 'id', headerName: 'DATE', width: 160, flex: 2 },
+  {
+    field: 'users',
+    headerName: 'Users',
+    width: 160,
+    align: 'right',
+    headerAlign: 'right',
+    flex: 2,
+  },
+  {
+    field: 'avgTimeperUser',
+    headerName: 'Avg Time per User',
+    description: '전체 체류 시간 / 유저 수',
+    width: 160,
+    align: 'right',
+    headerAlign: 'right',
+    valueFormatter: (params: CellParams) => `${params.value} s`,
+    flex: 4,
+  },
+  {
+    field: 'hits',
+    headerName: 'Hits',
+    type: 'number',
+    width: 160,
+    align: 'right',
+    headerAlign: 'right',
+    flex: 2,
+  },
+  {
+    field: 'avgTimeperHits',
+    headerName: 'Avg Time per Hits',
+    description: '전체 체류 시간 / 방문횟수',
+    width: 160,
+    align: 'right',
+    headerAlign: 'right',
+    valueFormatter: (params: CellParams) => `${params.value} s`,
+    flex: 4,
+  },
+];
 
 interface IPerDay {
   _id: {
@@ -22,6 +61,7 @@ interface IPerDay {
   sum_duration: number;
   avg_duration: number;
   count: number;
+  userCount: number;
 }
 interface IProps {
   pageDurationPerDay: IPerDay[];
@@ -29,39 +69,28 @@ interface IProps {
 
 function DailyDurationTable(props: IProps): React.ReactElement {
   const { pageDurationPerDay } = props;
+  const styles = useStyles();
   return (
     <Box my={3}>
-      <Box my={3}>
-        <TableContainer component={Paper}>
-          <Table aria-label="simple table">
-            <TableHead>
-              <TableRow>
-                <TableCell align="center">DATE</TableCell>
-                <TableCell align="center">
-                  Overall Time <small>(s)</small>
-                </TableCell>
-                <TableCell align="center">
-                  Avg Time <small>(s)</small>
-                </TableCell>
-                <TableCell align="center">Hits</TableCell>
-              </TableRow>
-            </TableHead>
-            <TableBody>
-              {pageDurationPerDay &&
-                pageDurationPerDay.map((row: IPerDay) => (
-                  <TableRow key={`${row._id.year}-${row._id.month}-${row._id.day}`}>
-                    <TableCell component="th" scope="row" align="center">
-                      {`${row._id.year}-${row._id.month}-${row._id.day}`}
-                    </TableCell>
-                    <TableCell align="center">{(row.sum_duration / 1000).toFixed(1)}</TableCell>
-                    <TableCell align="center">{(row.avg_duration / 1000).toFixed(1)}</TableCell>
-                    <TableCell align="center"> {row.count}</TableCell>
-                  </TableRow>
-                ))}
-            </TableBody>
-          </Table>
-        </TableContainer>
-      </Box>
+      <Typography variant="h3" id="tableTitle" component="div">
+        일별 체류시간
+      </Typography>
+      <div style={{ height: 400, width: '100%' }}>
+        <DataGrid
+          className={styles.table}
+          rows={pageDurationPerDay.map((row) => {
+            return {
+              id: `${row._id.year}-${row._id.month}-${row._id.day}`,
+              users: row.userCount,
+              avgTimeperUser: (row.sum_duration / row.userCount / 1000).toFixed(1),
+              hits: row.count,
+              avgTimeperHits: (row.avg_duration / 1000).toFixed(1),
+            };
+          })}
+          columns={columns}
+          pageSize={10}
+        />
+      </div>
     </Box>
   );
 }
--- src/components/Analysis/PageDurationTable.tsx
+++ src/components/Analysis/PageDurationTable.tsx
@@ -1,62 +1,94 @@
 import React from 'react';
-import {
-  Box,
-  Button,
-  Table,
-  Typography,
-  TableBody,
-  TableCell,
-  TableContainer,
-  TableHead,
-  TableRow,
-  Paper,
-} from '@material-ui/core';
+import { Box, Typography } from '@material-ui/core';
 
-import arrayToCSV from '../../utils/arrayToCSV';
+import { makeStyles } from '@material-ui/core/styles';
+import { DataGrid, ColDef, CellParams } from '@material-ui/data-grid';
+
+const useStyles = makeStyles({
+  table: {
+    bgColor: 'primary',
+    '&.MuiDataGrid-root .MuiDataGrid-colCellTitle': {
+      fontWeight: '600',
+    },
+  },
+});
+const columns: ColDef[] = [
+  { field: 'id', headerName: 'Path', width: 160, flex: 2 },
+  {
+    field: 'users',
+    headerName: 'Users',
+    width: 160,
+    align: 'right',
+    headerAlign: 'right',
+    flex: 2,
+  },
+  {
+    field: 'avgTimeperUser',
+    headerName: 'Avg Time per User',
+    description: '전체 체류 시간 / 유저 수',
+    width: 160,
+    align: 'right',
+    headerAlign: 'right',
+    valueFormatter: (params: CellParams) => `${params.value} s`,
+    flex: 4,
+  },
+  {
+    field: 'hits',
+    headerName: 'Hits',
+    type: 'number',
+    width: 160,
+    align: 'right',
+    headerAlign: 'right',
+    flex: 2,
+  },
+  {
+    field: 'avgTimeperHits',
+    headerName: 'Avg Time per Hits',
+    description: '전체 체류 시간 / 방문횟수',
+    width: 160,
+    align: 'right',
+    headerAlign: 'right',
+    valueFormatter: (params: CellParams) => `${params.value} s`,
+    flex: 4,
+  },
+];
 
 interface IDuration {
   _id: string;
   avg_duration: number;
   sum_duration: number;
   count: number;
+  userCount: number;
 }
 interface IProps {
   pageDurationData: IDuration[];
 }
 function SessionTable(props: IProps): React.ReactElement {
   const { pageDurationData } = props;
-
+  const styles = useStyles();
   return (
-    <Box mb={3}>
-      <TableContainer component={Paper}>
-        <Table aria-label="simple table">
-          <TableHead>
-            <TableRow>
-              <TableCell align="center">Path</TableCell>
-              <TableCell align="center">
-                Overall Time <small>(s)</small>
-              </TableCell>
-              <TableCell align="center">
-                Avg Time <small>(s)</small>
-              </TableCell>
-              <TableCell align="center">Hits</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {pageDurationData &&
-              pageDurationData.map((row: IDuration) => (
-                <TableRow key={row._id}>
-                  <TableCell component="th" scope="row" align="center">
-                    {row._id}
-                  </TableCell>
-                  <TableCell align="center">{(row.sum_duration / 1000).toFixed(1)}</TableCell>
-                  <TableCell align="center"> {(row.avg_duration / 1000).toFixed(1)}</TableCell>
-                  <TableCell align="center"> {row.count}</TableCell>
-                </TableRow>
-              ))}
-          </TableBody>
-        </Table>
-      </TableContainer>
+    <Box my={3}>
+      <Typography variant="h3" id="tableTitle" component="div">
+        페이지별 체류시간
+      </Typography>
+      <div style={{ height: 400, width: '100%' }}>
+        <DataGrid
+          className={styles.table}
+          rows={pageDurationData
+            .filter((row) => !!row._id)
+            .map((row) => {
+              return {
+                id: row._id,
+                users: row.userCount,
+                avgTimeperUser: (row.sum_duration / row.userCount / 1000).toFixed(1),
+                hits: row.count,
+                avgTimeperHits: (row.avg_duration / 1000).toFixed(1),
+              };
+            })}
+          columns={columns}
+          pageSize={10}
+        />
+      </div>
     </Box>
   );
 }
