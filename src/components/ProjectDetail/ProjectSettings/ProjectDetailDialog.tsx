c0420366a114fa9a91f88b142d347306c7e0b076
saeeng <saeeng.opt@gmail.com>
2020-12-17 10:55:55 +0900

    merge

--- src/components/ProjectDetail/ProjectSettings/ProjectDetailDialog.tsx
+++ src/components/ProjectDetail/ProjectSettings/ProjectDetailDialog.tsx
@@ -3,14 +3,8 @@ import { makeStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
 import Dialog from '@material-ui/core/Dialog';
 import Box from '@material-ui/core/Box';
-<<<<<<< HEAD:src/components/ProjectDetail/ProjectDetailDialog.tsx
-import CopyClipboardBox from '../common/CopyClipboardBox';
-import CodeSnippet from '../NewProject/CodeSnippet';
-=======
-import SyntaxHighlighter from 'react-syntax-highlighter';
-import { dark } from 'react-syntax-highlighter/dist/esm/styles/hljs';
+import CodeSnippet from '../../NewProject/CodeSnippet';
 import CopyClipboardBox from '../../common/CopyClipboardBox';
->>>>>>> develop:src/components/ProjectDetail/ProjectSettings/ProjectDetailDialog.tsx
 
 const useStyles = makeStyles({
   button: {
